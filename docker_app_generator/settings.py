cf4bb8b6297c284b8e03d1ba6ff39c2d8ff7c8f7
jc <jc@houseofagile.com>
2019-07-16 13:39:04 +0200

    Deal with docker_post extra actions

--- docker_app_generator/settings.py
+++ docker_app_generator/settings.py
@@ -5,4 +5,4 @@ assembly_path = 'apps-assembly'
 apps_path = './apps-universe'
 stacks_path = './stacks-universe'
 default_docker_network = 'hoa_network'
-docker_main_sections = ['docker_from', 'docker_init', 'docker_reqs', 'docker_core']
+docker_main_sections = ['docker_from', 'docker_init', 'docker_reqs', 'docker_core', 'docker_post']

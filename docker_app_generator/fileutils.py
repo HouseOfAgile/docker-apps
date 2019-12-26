9f56ad3682372a8c2963b5bb7a72d093f8bd1362
jc <jc@houseofagile.com>
2019-12-26 19:24:16 +0100

    Refactor copy_files copy config files by flavors/types Reorganize structure pylint

--- docker_app_generator/fileutils.py
+++ docker_app_generator/fileutils.py
@@ -12,11 +12,12 @@ resource_package = __name__
 
 
 env = Environment(
-    loader=PackageLoader('docker_app_generator', 'templates'),
-    autoescape=select_autoescape(['html', 'xml'])
+    loader=PackageLoader("docker_app_generator", "templates"),
+    autoescape=select_autoescape(["html", "xml"]),
 )
 
 pp = pprint.PrettyPrinter(indent=4)
+
 
 def load_config_file(filename):
     if is_valid_file(pkg_resources.resource_filename(resource_package, filename)):
@@ -24,56 +25,83 @@ def load_config_file(filename):
     else:
         return []
 
+
 def create_app_structure(path_app):
     bf.mkdir_p(path_app)
+    bf.mkdir_p(path_app + "/dockerfiles")
+
 
 def cleanup_directory(dir):
     if is_valid_directory(dir):
         shutil.rmtree(dir)
 
-def copy_files(from_path, to_path):
+
+def copy_files_old(from_path, to_path):
     copy_tree(from_path, to_path) if is_valid_file(from_path) else None
 
 
-def generate_app_dockerfile(path_app, docker_stack, docker_base, docker_flavors, variant_name=None):
-    # print('Dockerfile config {}\ndocker_base: {}\ndocker_stack: {}\n'.format(
-        # path_app, docker_base, docker_stack))
+def copy_files(from_path_base, stack_element, name, to_path_base):
+    # copy files
+    for file_type in ["config", "files"]:
+        from_path = from_path_base + "/" + stack_element + "/" + name + "/" + file_type
+        to_path = to_path_base + "/" + file_type + "/" + name
+        print("from {}, to {}".format(from_path, to_path))
+        copy_tree(from_path, to_path,) if is_valid_file(from_path) else None
 
-    template = env.get_template('Dockerfile.j2')
+    # copy config
+    # copy_tree(from_path, to_path) if is_valid_file(from_path) else None
+
+
+def generate_app_dockerfile(
+    path_app, docker_stack, docker_base, docker_flavors, variant_name=None
+):
+    # print('Dockerfile config {}\ndocker_base: {}\ndocker_stack: {}\n'.format(
+    # path_app, docker_base, docker_stack))
+
+    template = env.get_template("Dockerfile.j2")
     template.stream(
         docker_base=docker_base,
         docker_stack=docker_stack,
         docker_flavors=docker_flavors,
-    ).dump(path_app + '/Dockerfile' + ('.' + variant_name if variant_name else ''))
+    ).dump(
+        path_app
+        + "/dockerfiles/"
+        + (variant_name + "." if variant_name else "")
+        + "Dockerfile"
+    )
 
 
 def generate_build_shell(path_app, docker_image_name, docker_bases, settings):
     # print('Shell config\npath_app: {}\docker_image_name: {}\docker_bases: {}\n'.format(
-        # path_app, docker_image_name, docker_bases))
+    # path_app, docker_image_name, docker_bases))
 
-    template = env.get_template('build.sh.j2')
+    template = env.get_template("build.sh.j2")
     template.stream(
         settings=settings,
         docker_image_name=docker_image_name,
         docker_bases=docker_bases,
-    ).dump(path_app + '/build.sh')
+    ).dump(path_app + "/build.sh")
 
 
-def generate_app_docker_compose(path_app, service_config, service_main, service_definition, settings):
+def generate_app_docker_compose(
+    path_app, service_config, service_main, service_definition, settings
+):
     # print('compose {}\n{}\n{}\n{}\n###'.format(
-        # service_config, service_main, service_definition, myconfig))
+    # service_config, service_main, service_definition, myconfig))
 
-    template = env.get_template('docker-compose.yml.j2')
+    template = env.get_template("docker-compose.yml.j2")
     template.stream(
         service_config=service_config,
         service_main=service_main,
         service_definition=service_definition,
-        settings=settings
-    ).dump(path_app + '/docker-compose.yml')
+        settings=settings,
+    ).dump(path_app + "/docker-compose.yml")
+
 
 def is_valid_file(filename):
     p = Path(filename)
     return p.exists()
+
 
 def is_valid_directory(filename):
     p = Path(filename)
--- docker_app_generator/generator.py
+++ docker_app_generator/generator.py
@@ -6,103 +6,124 @@ from . import settings
 
 pp = pprint.PrettyPrinter(indent=4)
 
+
 def generate_docker_app(myconfig):
 
-    for app_config in myconfig['apps_inventory']:
-        path_app = settings.apps_path + '/' + app_config['name']
+    for app_config in myconfig["apps_inventory"]:
+        path_app = settings.apps_path + "/" + app_config["name"]
         # create dir
         fu.create_app_structure(path_app)
 
         # load_config
-        docker_stack = conf.load_config(
-            app_config['name'], 'apps')
-        docker_bases = conf.load_config(
-            app_config['docker_base'],   'bases') if app_config['docker_base'] else []
-        docker_flavors = conf.load_flavors_config(
-            app_config['docker_flavors'])
+        docker_stack = conf.load_config(app_config["name"], "apps")
+        docker_bases = (
+            conf.load_config(app_config["docker_base"], "bases")
+            if app_config["docker_base"]
+            else []
+        )
+        docker_flavors = conf.load_flavors_config(app_config["docker_flavors"])
 
         # generate each variant
         if docker_bases:
             # add main vars
-            docker_bases['variants'].append(
-                {'name': 'main', 'variant_vars': docker_bases['main_vars']})
+            docker_bases["variants"].append(
+                {"name": "main", "variant_vars": docker_bases["main_vars"]}
+            )
 
-            for variant in docker_bases['variants']:
+            for variant in docker_bases["variants"]:
                 # docker_base is the cleaned docker commands with replaced variants if they exist
                 docker_base = {}
                 # print('Variant {} with vars: {}'.format(variant['name'], variant['variant_vars']))
                 for element in settings.docker_main_sections:
-                    content = docker_bases['main'][element]
-                    if variant != 'main':
-                        for key in variant['variant_vars']:
-                            content = content.replace(
-                                key, variant['variant_vars'][key])
+                    content = docker_bases["main"][element]
+                    if variant != "main":
+                        for key in variant["variant_vars"]:
+                            content = content.replace(key, variant["variant_vars"][key])
                     else:
-                        for key in docker_bases['main_vars']:
+                        for key in docker_bases["main_vars"]:
                             content = content.replace(
-                                key, docker_bases['main_vars'][key])
+                                key, docker_bases["main_vars"][key]
+                            )
 
                     docker_base[element] = content
 
                 # merge all values
                 docker_base = dict(
-                    list(docker_bases['main'].items()) + list(docker_base.items()))
+                    list(docker_bases["main"].items()) + list(docker_base.items())
+                )
                 # update dynamic parameters
-                docker_stack = update_parameters(
-                    docker_stack, myconfig['parameters'])
-                docker_base = update_parameters(
-                    docker_base, myconfig['parameters'])                    
+                docker_stack = update_parameters(docker_stack, myconfig["parameters"])
+                docker_base = update_parameters(docker_base, myconfig["parameters"])
                 for flavor in docker_flavors:
                     docker_flavors[flavor] = update_parameters(
-                        docker_flavors[flavor], myconfig['parameters'])
+                        docker_flavors[flavor], myconfig["parameters"]
+                    )
 
-                fu.generate_app_dockerfile(path_app, docker_stack, docker_base,
-                                           docker_flavors, variant['name'] if variant['name'] != 'main' else None)
-                image_variants = [v['name']
-                                  for v in docker_bases['variants'] if v['name'] != 'main']
-                image_name = app_config['image_name'] if app_config['image_name'] else app_config['name']
-                fu.generate_build_shell(
-                    path_app, image_name, image_variants, settings)
-                print("Generated docker apps for %s:%s" %
-                      (app_config['name'], variant['name']))
+                fu.generate_app_dockerfile(
+                    path_app,
+                    docker_stack,
+                    docker_base,
+                    docker_flavors,
+                    variant["name"] if variant["name"] != "main" else None,
+                )
+                image_variants = [
+                    v["name"] for v in docker_bases["variants"] if v["name"] != "main"
+                ]
+                image_name = (
+                    app_config["image_name"]
+                    if "image_name" in app_config
+                    else app_config["name"]
+                )
+                fu.generate_build_shell(path_app, image_name, image_variants, settings)
+                print(
+                    "Generated docker apps for %s:%s"
+                    % (app_config["name"], variant["name"])
+                )
         else:
-            print('base is empty')
+            print("base is empty")
 
         # copy files
-        full_assembly_path = settings.current_path + \
-            os.path.join(os.path.sep, settings.assembly_path)
+        full_assembly_path = settings.current_path + os.path.join(
+            os.path.sep, settings.assembly_path
+        )
 
-        fu.copy_files(full_assembly_path + '/apps/' +
-                      app_config['name'] + '/files', path_app + '/files/' + app_config['name'])
-        fu.copy_files(full_assembly_path + '/bases/' +
-                      app_config['docker_base'] + '/files', path_app + '/files/' + app_config['docker_base'])
-        for flavor in app_config['docker_flavors']:
-            fu.copy_files(full_assembly_path + '/flavors/' + flavor + '/files',
-                          path_app + '/files/' + flavor)
+        fu.copy_files(full_assembly_path, "apps", app_config["name"], path_app)
+        fu.copy_files(full_assembly_path, "bases", app_config["docker_base"], path_app)
+        for flavor in app_config["docker_flavors"]:
+            fu.copy_files(full_assembly_path, "flavors", flavor, path_app)
 
         # pp.pprint(myconfig['assemblies'][app_config['name']])
 
-    for stack_config in myconfig['stacks_inventory']:
-        stack_path = settings.stacks_path + '/' + stack_config['name']
+    for stack_config in myconfig["stacks_inventory"]:
+        stack_path = settings.stacks_path + "/" + stack_config["name"]
         # create dir
         fu.create_app_structure(stack_path)
         # load service config
-        service_main = conf.load_config(
-            stack_config['app_main_service'], 'bases', 'stacks') if app_config['docker_base'] else []
+        service_main = (
+            conf.load_config(stack_config["app_main_service"], "bases", "stacks")
+            if app_config["docker_base"]
+            else []
+        )
         service_definition = conf.load_flavors_config(
-            stack_config['app_other_services'], 'stacks')
+            stack_config["app_other_services"], "stacks"
+        )
         # generate docker compose
-        fu.generate_app_docker_compose(stack_path, stack_config,
-                                       service_main, service_definition, settings)
+        print("service_main {}".format(service_main))
+        print("definition {}".format(service_definition))
+        fu.generate_app_docker_compose(
+            stack_path, stack_config, service_main, service_definition, settings
+        )
+        print("Generated docker stack for %s" % (stack_config["name"]))
 
 
 def update_parameters(yaml_dict, parameters):
-    if(parameters):
+    if parameters:
         for element in yaml_dict:
-            if element != 'docker_expose':
+            if element != "docker_expose":
                 for parameter in parameters:
                     yaml_dict[element] = yaml_dict[element].replace(
-                        '#'+parameter+'#', parameters[parameter]) 
+                        "#" + parameter + "#", parameters[parameter]
+                    )
 
     return yaml_dict
 
@@ -110,5 +131,5 @@ def update_parameters(yaml_dict, parameters):
 def cleanup_docker_app(myconfig):
     fu.cleanup_directory(settings.apps_path)
     fu.cleanup_directory(settings.stacks_path)
-    print('Apps and stacks path has been cleaned up')
+    print("Apps and stacks path has been cleaned up")
 
